eaedd16f5304b40b09fbf173f6ee300a7f8c1ea8
Michael Hernandez <michaelhernandezrala@gmail.com>
2023-09-19 14:16:47 +0200

    Some directories removal

--- src/main/java/src/java/connect4/Turn.java
+++ src/main/java/src/java/connect4/Turn.java
@@ -3,8 +3,11 @@ package src.java.connect4;
 class Turn {
 
     private Board board;
+    private Player[] players;
+    private static final int NUMBER_PLAYERS = 2;
 
     Turn(Board board){
         this.board = board;
+        this.players = new Player[NUMBER_PLAYERS];
     }
 }
